dde39fdc24640cd7b0c58c6362fe9fbe0dc8fcaa
Christian D Tuen <christiandt@gmail.com>
2014-02-05 15:51:51 -0800

    The program now reads and writes the file via an int-array

--- simulator.py
+++ simulator.py
@@ -1,1 +1,24 @@
 __author__ = 'christue'
+
+
+
+def readFile():
+    byteFile = []
+    with open('pink_panther.au', 'rb') as musicFile:
+        while True:
+            byte = musicFile.read(1).encode('hex')
+            if not byte:
+                break
+            byteFile.append(int(byte, 16))
+    musicFile.close()
+    return byteFile
+
+
+def writeFile(byteFile):
+    with open('destFile.au', 'wb') as musicFile:
+        musicFile.write(bytearray(byteFile))
+    musicFile.close()
+
+
+filearray =readFile()
+writeFile(filearray)
