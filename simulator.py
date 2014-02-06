0ddaf8b8ab682c7ac4f48e245b76e67cc0690447
Christian D Tuen <christiandt@gmail.com>
2014-02-05 18:11:08 -0800

    The program now has all the classes and is able to simulate loss ove a network

--- simulator.py
+++ simulator.py
@@ -1,24 +1,94 @@
 __author__ = 'christue'
 
+import random
+import copy
 
 
-def readFile():
-    byteFile = []
-    with open('pink_panther.au', 'rb') as musicFile:
-        while True:
-            byte = musicFile.read(1).encode('hex')
-            if not byte:
-                break
-            byteFile.append(int(byte, 16))
-    musicFile.close()
-    return byteFile
+class Server():
+    global packetSize
+    packetSize = 100
+
+    def streamFile(self):
+        network = Network()
+        with open('pink_panther.au', 'rb') as musicFile:
+            eof = False
+            while not eof:
+                bytes = []
+                for i in range(packetSize):
+                    byte = musicFile.read(1).encode('hex')
+                    if not byte:
+                        eof = True
+                        break
+                    bytes.append(int(byte, 16))
+                packet = Packet(bytes)
+                network.send(packet)
+        musicFile.close()
 
 
-def writeFile(byteFile):
-    with open('destFile.au', 'wb') as musicFile:
-        musicFile.write(bytearray(byteFile))
-    musicFile.close()
+class Packet():
+    global counter
+    counter = 0
+
+    def __init__(self, bytes):
+        global counter
+        self.bytes = bytes
+        self.number = counter
+        counter += 1
 
 
-filearray =readFile()
-writeFile(filearray)
+class Network():
+    global threshold
+    threshold = 75
+    clients = []
+
+    def connect(self, client):
+        self.clients.append(client)
+
+    def send(self, packet):
+        loss = random.randint(1, 100)
+        if loss <= threshold:
+            for client in self.clients:
+                client.receiveStream(packet)
+
+
+class Client():
+    global silence
+    silence = True
+    packets = []
+
+    def connect(self):
+        self.network = Network()
+        self.network.connect(self)
+
+    def receiveStream(self, packet):
+        #packet.bytes = [255] * len(packet.bytes)
+        if len(self.packets) > 0:
+            lostPackets = (packet.number - self.packets[-1].number)-1
+            print "Lost %s packets" % lostPackets
+            for i in range(lostPackets):
+                lastPacket = self.packets[-1]
+                if silence:
+                    silentPacket = copy.deepcopy(lastPacket)
+                    silentPacket.bytes = [255] * len(lastPacket.bytes)
+                    self.packets.append(silentPacket)
+                else:
+                    self.packets.append(lastPacket)
+
+        self.packets.append(packet)
+
+    def writeFile(self):
+        self.byteFile = []
+        for packet in self.packets:
+            for byte in packet.bytes:
+                self.byteFile.append(byte)
+        with open('received_file.au', 'wb') as musicFile:
+            musicFile.write(bytearray(self.byteFile))
+        musicFile.close()
+
+
+client = Client()
+server = Server()
+
+client.connect()
+server.streamFile()
+client.writeFile()
